458c17ba1ef2684901396ba794ea2a2a6006d3e0
Riccardo La Torre <riccardo.latorre@acuris.com>
2020-09-10 16:18:39 +0100

    HelloPoint2

--- src/chapter2/index.js
+++ src/chapter2/index.js
@@ -2,7 +2,7 @@ import {  Matrix4, Vector3, Vector4 } from '../lib/cuon-matrix';
 import { initShaders, createProgram, loadShader, getWebGLContext } from '../lib/cuon-utils';
 import WebGLDebugUtils from '../lib/webgl-debug';
 import WebGLUtils from '../lib/webgl-utils';
-import vshader from './HelloPoint1.vert';
+import vshader from './HelloPoint2.vert';
 import fshader from './HelloPoint1.frag';
 
 function main() {
@@ -14,6 +14,15 @@ function main() {
     console.log('Failed to init shaders');
     return;
   }
+
+  let a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+
+  if (a_Position < 0) {
+    console.log('Failed to get storage location of a_Position');
+    return;
+  }
+
+  gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
 
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
